8b2bc3de6f73069f1882d1ddd64dd8f8add21c78
Arihant Tonage <arihanttonage@gmail.com>
2023-01-26 14:11:42 +0530

    persistent theme implemented

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,25 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:mytasks/objects/to_do_item.dart';
 import 'package:mytasks/pages/home_page.dart';
 import 'package:mytasks/providers/theme_provider.dart';
+import 'package:mytasks/theme/pallete.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter(ToDoItemAdapter());
   var box = await Hive.openBox('tasksbox');
-  runApp(const MyApp());
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  if (!prefs.containsKey('isDark')) {
+    await prefs.setBool('isDark', false);
+  }
+  bool isDark = prefs.getBool('isDark')!;
+  Pallete.changedTheme = isDark;
+  return runApp(ChangeNotifierProvider(
+    create: ((context) => ThemeProvider(isDark: isDark)),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -18,16 +29,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: ((context) => ThemeProvider()),
-      builder: ((context, child) {
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
-          theme: Provider.of<ThemeProvider>(context).theme,
+          theme: themeProvider.theme,
           home: const HomePage(),
         );
-      }),
+      },
     );
   }
 }
